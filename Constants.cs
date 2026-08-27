d534639a92d07adbd94c29d018b6f909081ff7e9
lolca_f <james.p.benton@icloud.com>
2026-08-27 17:13:59 -0400

    Embedded Assetbundle Update

--- Constants.cs
+++ Constants.cs
@@ -5,5 +5,5 @@ public static class Constants
     public const string Name = "GorillaMusicPad";
     public const string Guid = "com.lolcaf.gorillamusicpad";
     public const string Author = "lolcaf";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 }
