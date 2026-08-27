80113566f6c9174d00970f3b3ad3b0916f77d0dd
lolca_f <james.p.benton@icloud.com>
2026-08-27 17:15:21 -0400

    Embedded Assetbundle Update

--- Main.cs
+++ Main.cs
@@ -1,10 +1,9 @@
+using GorillaMapTeleporter.Utilities;
 using GorillaMusicPad.Classes;
 using GorillaMusicPad.MonoBehaviors;
 using GorillaMusicPad.Patches;
 using GorillaMusicPad.Utilities;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace GorillaMusicPad;
 
@@ -40,39 +39,12 @@ public class Main : MonoBehaviour
     // This is called when everything is ready in the game before the gorilla is spawned into the world.
     private void OnPlayerSpawned()
     {
-        StartCoroutine(LoadAssetBundle());
+        LoadAssetBundle();
     }
 
-    private IEnumerator LoadAssetBundle()
+    private void LoadAssetBundle()
     {
-        string weburl = "https://github.com/lolcaf/GorillaMusicPad-GT/raw/refs/heads/main/Resources/AssetBundle/gorillamusicpad";
-        Log.WriteLine("Loading the assetbundle from the web");
-        using (UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(weburl))
-        {
-            yield return www.SendWebRequest();
-            if (www.result == UnityWebRequest.Result.Success)
-            {
-                AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
-                GameObject go = (GameObject)bundle.LoadAsset(bundle.GetAllAssetNames()[0]);
-                if (go != null)
-                {
-                    theMenu = Instantiate(go);
-                    theMenu.AddComponent<MenuManager>();
-                    theMenu.transform.localScale = Vector3.one * 1.5f;
-                    Log.WriteLine($"the menu is {theMenu}");
-                }
-                else
-                {
-                    Log.WriteLine("Asset bundle game object is null");
-                }
-                bundle.Unload(false);
-                yield return new WaitForEndOfFrame();
-            }
-            else
-            {
-                Log.WriteLine($"Failed to load asset bundle from web: {www.error}");
-            }
-            www.Dispose();
-        }
+        GameObject menu = AssetBundleUtilities.Load("GorillaMusicPad.Resources.gorillamusicpad", "MusicPad");
+        Instantiate(menu).AddComponent<MenuManager>();
     }
 }
